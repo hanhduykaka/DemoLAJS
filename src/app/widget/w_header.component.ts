69d02093bf9c5e6197897cf4c7bb38c3ecec8661
hanhduykaka <hanhduykaka@users.noreply.github.com>
2017-11-08 23:12:51 +0700

    Add files via upload

---
+++ src/app/widget/w_header.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'w_header',
+  templateUrl: './w_header.component.html',
+})
+export class WheaderComponent implements OnInit  {
+    ngOnInit() {
+      }
+}
