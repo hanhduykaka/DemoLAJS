9c3253e97be246e3a491e98d69479b7d6c1c6119
hanhduykaka <hanhduykaka@users.noreply.github.com>
2017-11-08 23:09:29 +0700

    Add files via upload

---
+++ src/app/widget/w_footer/bottom_footer/bottom_footer.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bottom_footer',
+  templateUrl: './bottom_footer.component.html',
+})
+export class BottomfooterComponent implements OnInit  {
+    ngOnInit() {
+      }
+}
