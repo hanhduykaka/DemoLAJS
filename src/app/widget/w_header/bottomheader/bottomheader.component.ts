9caec8e86d7ae25055d61881521b24a405076f7d
hanhduykaka <hanhduykaka@users.noreply.github.com>
2017-11-08 23:11:09 +0700

    Add files via upload

---
+++ src/app/widget/w_header/bottomheader/bottomheader.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bottom_header',
+  templateUrl: './bottomheader.component.html',
+})
+export class BottomComponent implements OnInit  {
+    ngOnInit() {
+      }
+}
